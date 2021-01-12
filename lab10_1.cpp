b656e46b7de39cd2a04d626897f30425b090d33f
phurin2644 <phus.2644@gmail.com>
2021-01-12 16:40:37 +0700

    lab10_1com

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,17 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime> 
+using namespace std;
+
+int main(){
+	cout << "Press Enter 3 times to reveal your future.";
+	for(int i = 0; i < 3; i++){
+		cin.get();
+	}
+	srand(time(0));
+	int x = rand()%9;
+	string v[] = {"A","B+","B","C+","C","D+","D","F","W"};
+	cout << "You will get " << v[x] << " in this 261102."; 
+	return 0;
+}
